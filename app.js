fd3657ac2f69ae93a8f5c646a07ff84a78e9d0db
Milisiita <janinacaceres23@gmail.com>
2024-05-10 23:34:11 +0200

    Primera Version

---
+++ app.js
@@ -0,0 +1,11 @@
+const coberturas = document.getElementsByClassName('cobertura')
+
+
+function mostrarClic (e){
+    console.log(e.target.innerText);
+}
+for (const cobertura of coberturas){
+    cobertura.addEventListener('click',mostrarClic);
+
+}
+    
